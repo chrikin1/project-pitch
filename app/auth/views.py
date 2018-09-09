fe6ad0315cb22bc33b1e823488c8576d779feb71
chrikin1 <chrisianbartoo@gmail.com>
2018-09-09 23:44:49 +0300

    initial commit

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,6 +6,18 @@ from .. import db
 from flask_login import login_user, logout_user, login_required
 from ..email import mail_message
 
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email=login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    return render_template('auth/login.html', login_form=login_form)
 
 @auth.route('/register', methods=["GET", "POST"])
 def register():
