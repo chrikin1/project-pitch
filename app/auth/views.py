ed733475039995f6d97ba7ee6f82a667658dc1be
chrikin1 <chrisianbartoo@gmail.com>
2018-09-09 23:43:57 +0300

    initial commit

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,3 +5,16 @@ from .forms import RegistrationForm, LoginForm
 from .. import db
 from flask_login import login_user, logout_user, login_required
 from ..email import mail_message
+
+
+@auth.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html', registration_form=form)
