6069b650c07b474976b7ab6aa06f1f4afd220b64
chrikin1 <chrisianbartoo@gmail.com>
2018-09-10 08:09:59 +0300

    initial commit

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
 	MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
 	MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 	SUBJECT_PREFIX = 'PITCH'
-	SENDER_EMAIL = 'benkaranja43@gmail.com'
+	SENDER_EMAIL = 'chrisbartoo@gmail.com'
 
 
 
