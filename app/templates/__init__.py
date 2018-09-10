909cb000d6fd691e1afe25fbfa4e82c21334981c
chrikin1 <chrisianbartoo@gmail.com>
2018-09-10 07:24:51 +0300

    good commit

--- app/templates/__init__.py
+++ app/templates/__init__.py
@@ -19,3 +19,11 @@ def create_app(config_name):
 	login_manager.login_view = 'auth.login'
 	configure_uploads(app,photos)
 	mail.init_app(app)
+
+frfrom .main import main as main_blueprint
+	app.register_blueprint(main_blueprint)
+
+	from .auth import auth as auth_blueprint
+	app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
+
+	return app
