e3784cd5ffaaf6c4e7c176f17b4eb83e05f29c59
chrikin1 <chrisianbartoo@gmail.com>
2018-09-11 07:32:28 +0300

    added changes

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask_ import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
-from wtforms.validators import Required, Email, EqualTo
+from forms.validators import Required, Email, EqualTo
 from ..models import User
 from wtforms import ValidationError
 
